8b155262a5789d2b8ae9eda8d0668186b20b8e73
Arctic Kona <kona@kona.akona.me>
2021-04-21 14:02:24 -0400

    new cookie

--- phone.go
+++ phone.go
@@ -8,9 +8,9 @@ import "os"
 
 // TextNow Configs
 var TextApiurl string = ternary( os.Getenv( "AKONA_TEXTNOW_APIURL" ) != "" , os.Getenv( "AKONA_TEXTNOW_APIURL" ) ,
-	"https://www.textnow.com/api/users/yuh1ilp_ugwncpimtcjh/messages" ).( string )
+	"https://www.textnow.com/api/users/mdd55svnlr3pqkeq5iq9/messages" ).( string )
 var TextCookie string = ternary( os.Getenv( "AKONA_TEXTNOW_COOKIE" ) != "" , os.Getenv( "AKONA_TEXTNOW_COOKIE" ) ,
-	"connect.sid=s:MYNJ16gBIZJXc0FWXlTY3p-2k2Tw1odW.qYUDZ6Qx4PN27GkyLxwT+3g+EheG0RuwphT5Qs1e0Kc;" ).( string )
+	"connect.sid=s:8x1SgmtNw4eNBbA8OQ0mtQqq07DivwAk.LmPINML7UXkusEDqGr/vIvt7qn9/tfcEcvjv/QgWRb8;" ).( string )
 
 // Sends out messages with TextNow. Probably illegal.
 func ( self Message )Text( ) error {
