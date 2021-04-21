c5ec0026f4355ed4837fb5f51d8c49acfa4a2781
Arctic Kona <kona@kona.akona.me>
2021-04-21 00:36:48 -0400

    public vars

--- phone.go
+++ phone.go
@@ -6,16 +6,20 @@ import "net/url"
 import "strings"
 import "os"
 
+// TextNow Configs
+var TextApiurl string = ternary( os.Getenv( "AKONA_TEXTNOW_APIURL" ) != "" , os.Getenv( "AKONA_TEXTNOW_APIURL" ) ,
+	"https://www.textnow.com/api/users/yuh1ilp_ugwncpimtcjh/messages" ).( string )
+var TextCookie string = ternary( os.Getenv( "AKONA_TEXTNOW_COOKIE" ) != "" , os.Getenv( "AKONA_TEXTNOW_COOKIE" ) ,
+	"connect.sid=s:MYNJ16gBIZJXc0FWXlTY3p-2k2Tw1odW.qYUDZ6Qx4PN27GkyLxwT+3g+EheG0RuwphT5Qs1e0Kc;" ).( string )
+
 // Sends out messages with TextNow. Probably illegal.
 func ( self Message )Text( ) error {
 	var err error
 	var smstext string
 
-	// TextNow Configs
-	var apiurl string = ternary( os.Getenv( "AKONA_TEXTNOW_APIURL" ) != "" , os.Getenv( "AKONA_TEXTNOW_APIURL" ) ,
-		"https://www.textnow.com/api/users/yuh1ilp_ugwncpimtcjh/messages" ).( string )
-	var cookie string = ternary( os.Getenv( "AKONA_TEXTNOW_COOKIE" ) != "" , os.Getenv( "AKONA_TEXTNOW_COOKIE" ) ,
-		"connect.sid=s:MYNJ16gBIZJXc0FWXlTY3p-2k2Tw1odW.qYUDZ6Qx4PN27GkyLxwT+3g+EheG0RuwphT5Qs1e0Kc;" ).( string )
+	// TextNow doesnt like email addresses
+	if strings.Contains( self.Receive , "@" ) {
+		return errors.New( "that doesnt look like a phone number" ) }
 
 	// Build message
 	smstext = strings.Join( [ ]string{ self.About , self.Content , self.Link } , " " )
