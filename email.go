fb2fb3ad85195bd0727b2130483b9fe4b29b5657
Arctic Kona <kona@kona.akona.me>
2021-04-21 22:30:26 -0400

    fix

--- email.go
+++ email.go
@@ -17,10 +17,11 @@ func ( self Message )Email( ) error {
 
 	// Operate templates
 	sendtext = self.Content + " " + self.Link
-	template , err := os.Open( "email.htm" )
-	if err != nil {
-		return err }
-	sendhtml = string( mu( ioutil.ReadAll( template ) )[ 0 ].( [ ]byte ) )
+//	template , err := os.Open( "email.htm" )
+//	if err != nil {
+//		return err }
+//	sendhtml = string( mu( ioutil.ReadAll( template ) )[ 0 ].( [ ]byte ) )
+	sendhtml = "<!DOCTYPE HTML><html><body><div style="color: white; background: black; font-size: 18px; font-family: sans-serif; padding: 48px; text-align: center;"><img src="https://media.akona.me/logo%20email.png" style="max-width: 480px;" alt="logo"><h1 style="font-size: 48px; font-family: Comfortaa, Quicksand, sans-serif; margin-top: 0px;">akona</h1><p>{{CONTENT}}<br/><a href="{{LINK}}" style="text-decoration: none;">{{LINK}}</a></p><p style="color: grey; font-size: 12;">If you did not make this request, account may be vulnerable.</p></div></body></html>"	// Hard code
 	sendhtml = strings.Replace( strings.Replace( sendhtml , "{{LINK}}" , self.Link , -1 ) , "{{CONTENT}}" , self.Content , -1 )
 
 	// Send
